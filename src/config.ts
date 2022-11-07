299eff69b988d8021d1a0ea3e7ad18f01c4fc51e
Xavier Morell <97970292+Xavimc04@users.noreply.github.com>
2022-11-07 12:01:37 +0100

    Update...

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const Config = {
     serverPort: process.env.PORT || 5502, 
-    connectionString: "mongodb+srv://admin:root@cluster0.5uoqj.mongodb.net/MyTicket?retryWrites=true&w=majority"
+    connectionString: "MONGO DB CONNECTION"
 }
 
 export default Config 
